1d36663e9027495ab43214a793d0cc8199c3469b
9p6b <9p6b@fresnou.org>
2021-11-08 08:25:22 -0800

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+router.post('/results', function (req, res){
+  console.log(req.body.description);
+  console.log(req.body.firstName);
+  console.log(req.body.type);
+  console.log(req.body.dateOfBirth);
+  console.log(req.body.Yes);
+  console.log(req.body.No);
+  res.render('index')
+})
+
 
 module.exports = router;
